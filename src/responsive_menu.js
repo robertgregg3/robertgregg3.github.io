53c18cce430a7c0b2a69e6ca5e0d27c6db56236b
Robert Gregg <hi@robertgregg.co.uk>
2024-11-04 13:14:26 +0000

    feat: update root url for github.io pages

--- src/responsive_menu.js
+++ src/responsive_menu.js
@@ -1,73 +1,78 @@
-const mainNav        = document.getElementById('nav-container');
-const inputName      = document.getElementById('name');
-const inputEmail     = document.getElementById('email');
-const inputTextarea  = document.getElementById('textarea');
-const statusName     = document.getElementById('status-name');
-const statusEmail    = document.getElementById('status-email');
-const statusTextarea = document.getElementById('status-textarea');
-const submitBtn      = document.getElementById('submit-btn');	
+const mainNav = document.getElementById("nav-container");
+const inputName = document.getElementById("name");
+const inputEmail = document.getElementById("email");
+const inputTextarea = document.getElementById("textarea");
+const statusName = document.getElementById("status-name");
+const statusEmail = document.getElementById("status-email");
+const statusTextarea = document.getElementById("status-textarea");
+const submitBtn = document.getElementById("submit-btn");
 
-let root = 'http://www.robertgregg.co.uk/';
+let root = "https://robertgregg3.github.io/";
 
 const menuData = [
-	{
-		page_title: 'Home',
-		url: root,
-		title: 'The home of Robert Gregg the Software Developer'
-	},
-	{
-		page_title: 'Portfolio',
-		url: root + `portfolio.html`,
-		title: 'View my portfolio'
-	},
-	{
-		page_title: 'CV',
-		url: root + `images/pdf/cv.pdf`,
-		alt: 'View My CV!'
-	},
-	{
-		page_title: 'Contact',
-		url: root + `contact.html`,
-		title: 'Get in touch!'
-	}
+  {
+    page_title: "Home",
+    url: root,
+    title: "The home of Robert Gregg the Software Developer",
+  },
+  {
+    page_title: "Portfolio",
+    url: root + `portfolio.html`,
+    title: "View my portfolio",
+  },
+  {
+    page_title: "CV",
+    url: root + `images/pdf/cv.pdf`,
+    alt: "View My CV!",
+  },
+  {
+    page_title: "Contact",
+    url: root + `contact.html`,
+    title: "Get in touch!",
+  },
 ];
 
 function toggleNav() {
-	const nav              = document.querySelector('nav .main-nav');
-	const getInTouchBtn    = document.getElementById('get-in-touch');
-	const navToggle        = document.querySelector('nav .nav__toggle');
-	const toggleTopLine    = document.querySelector('nav .nav__toggle__line-top');
-	const toggleMiddleLine = document.querySelector('nav .nav__toggle__line-middle')
-	const toggleBottomLine = document.querySelector('nav .nav__toggle__line-bottom');
-	
-	navToggle.addEventListener('click', toggleMenu);
+  const nav = document.querySelector("nav .main-nav");
+  const getInTouchBtn = document.getElementById("get-in-touch");
+  const navToggle = document.querySelector("nav .nav__toggle");
+  const toggleTopLine = document.querySelector("nav .nav__toggle__line-top");
+  const toggleMiddleLine = document.querySelector(
+    "nav .nav__toggle__line-middle"
+  );
+  const toggleBottomLine = document.querySelector(
+    "nav .nav__toggle__line-bottom"
+  );
 
-	if(getInTouchBtn){ // this button is just on the home page
-		getInTouchBtn.addEventListener('click', () => {
-			toggleMenu();
-			triggerContact();
-			nav.classList.add('get-in-touch-pressed');
-		});
-	}
+  navToggle.addEventListener("click", toggleMenu);
 
-	function toggleMenu() {
-		nav.classList.toggle('display');
-		toggleTopLine.classList.toggle('nav__toggle__line-top--selected');
-		toggleBottomLine.classList.toggle('nav__toggle__line-bottom--selected');
-		toggleMiddleLine.classList.toggle('display');
-		if(nav.classList.contains('get-in-touch-pressed')){
-			triggerContact();
-			nav.classList.remove('get-in-touch-pressed');
-		}
-	};
+  if (getInTouchBtn) {
+    // this button is just on the home page
+    getInTouchBtn.addEventListener("click", () => {
+      toggleMenu();
+      triggerContact();
+      nav.classList.add("get-in-touch-pressed");
+    });
+  }
+
+  function toggleMenu() {
+    nav.classList.toggle("display");
+    toggleTopLine.classList.toggle("nav__toggle__line-top--selected");
+    toggleBottomLine.classList.toggle("nav__toggle__line-bottom--selected");
+    toggleMiddleLine.classList.toggle("display");
+    if (nav.classList.contains("get-in-touch-pressed")) {
+      triggerContact();
+      nav.classList.remove("get-in-touch-pressed");
+    }
+  }
 }
 
 function addMenuData(menuData) {
-	const menuEl = document.createElement('div');
-	menuEl.classList.add('main-nav')
-	menuEl.classList.add('display')
+  const menuEl = document.createElement("div");
+  menuEl.classList.add("main-nav");
+  menuEl.classList.add("display");
 
-	menuEl.innerHTML = `
+  menuEl.innerHTML = `
 			<div class="nav-popup-left">
 				<div class="rob-head-shot"><img src="/images/rob.jpg" /></div>
 				<div id="contact-icons">
@@ -100,78 +105,79 @@ function addMenuData(menuData) {
 			</div>
 				`;
 
-	mainNav.appendChild(menuEl);
+  mainNav.appendChild(menuEl);
 
-	const contactBtn = document.getElementById('contact-button');
-	contactBtn.addEventListener('click', triggerContact);
+  const contactBtn = document.getElementById("contact-button");
+  contactBtn.addEventListener("click", triggerContact);
 
-	toggleNav();
+  toggleNav();
 }
 
-function triggerContact(){
-	const contactContainer = document.getElementById('contact-container');
-	const menuItems        = document.querySelectorAll('.nav-item'); 
-	const menuItemArrow    = document.querySelector('.nav-item-arrow'); 
-	const formInputs       = document.querySelectorAll('input')
+function triggerContact() {
+  const contactContainer = document.getElementById("contact-container");
+  const menuItems = document.querySelectorAll(".nav-item");
+  const menuItemArrow = document.querySelector(".nav-item-arrow");
+  const formInputs = document.querySelectorAll("input");
 
-	contactContainer.classList.toggle('hidden');
-	menuItemArrow.classList.toggle('point-down');
+  contactContainer.classList.toggle("hidden");
+  menuItemArrow.classList.toggle("point-down");
 
-	menuItems.forEach(item => {
-		item.classList.toggle('hidden');
+  menuItems.forEach((item) => {
+    item.classList.toggle("hidden");
 
-		formInputs.forEach(input => {
-			input.classList.toggle('form-resp-padding');
-		});
-	});
+    formInputs.forEach((input) => {
+      input.classList.toggle("form-resp-padding");
+    });
+  });
 }
 
 addMenuData(menuData);
 
-window.addEventListener("DOMContentLoaded", function() {
+window.addEventListener("DOMContentLoaded", function () {
+  // get the form elements defined in your form HTML above
 
-    // get the form elements defined in your form HTML above
-    
-    var form   = document.getElementById("contact-form");
-    var status = document.getElementById("my-form-status");
+  var form = document.getElementById("contact-form");
+  var status = document.getElementById("my-form-status");
 
-    // Success and Error functions for after the form is submitted
-    
-    function success() {
-      form.reset();
-      submitBtn.style = "display: none ";
-	  status.innerHTML = "Thanks! Your form was submitted successfully. I will usually respond within 24 hours";
-	  inputName.style = "display: none";
-	  inputEmail.style = "display: none";
-	  inputTextarea.style = "display: none";
-    }
+  // Success and Error functions for after the form is submitted
 
-    function error() {
-      status.innerHTML = "Oops! There was a problem.  Please check that you have filled in the form correctly";
-    }
-
-    // handle the form submission event
-
-    form.addEventListener("submit", function(ev) {
-      ev.preventDefault();
-      var data = new FormData(form);
-      ajax(form.method, form.action, data, success, error);
-    });
-  });
-  
-  // helper function for sending an AJAX request
-
-  function ajax(method, url, data, success, error) {
-    var xhr = new XMLHttpRequest();
-    xhr.open(method, url);
-    xhr.setRequestHeader("Accept", "application/json");
-    xhr.onreadystatechange = function() {
-      if (xhr.readyState !== XMLHttpRequest.DONE) return;
-      if (xhr.status === 200) {
-        success(xhr.response, xhr.responseType);
-      } else {
-        error(xhr.status, xhr.response, xhr.responseType);
-      }
-    };
-    xhr.send(data);
+  function success() {
+    form.reset();
+    submitBtn.style = "display: none ";
+    status.innerHTML =
+      "Thanks! Your form was submitted successfully. I will usually respond within 24 hours";
+    inputName.style = "display: none";
+    inputEmail.style = "display: none";
+    inputTextarea.style = "display: none";
   }
+
+  function error() {
+    status.innerHTML =
+      "Oops! There was a problem.  Please check that you have filled in the form correctly";
+  }
+
+  // handle the form submission event
+
+  form.addEventListener("submit", function (ev) {
+    ev.preventDefault();
+    var data = new FormData(form);
+    ajax(form.method, form.action, data, success, error);
+  });
+});
+
+// helper function for sending an AJAX request
+
+function ajax(method, url, data, success, error) {
+  var xhr = new XMLHttpRequest();
+  xhr.open(method, url);
+  xhr.setRequestHeader("Accept", "application/json");
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState !== XMLHttpRequest.DONE) return;
+    if (xhr.status === 200) {
+      success(xhr.response, xhr.responseType);
+    } else {
+      error(xhr.status, xhr.response, xhr.responseType);
+    }
+  };
+  xhr.send(data);
+}
